5aa242f81cef5887cce9be270160bcb2e83a895b
ndq04 <duyquang412000@gmail.com>
2021-11-27 11:29:41 +0700

    new update

--- server/index.js
+++ server/index.js
@@ -4,11 +4,24 @@ PORT = process.env.PORT || 8080
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const mongoose = require('./connect')
+
+const userRoute = require('./routes/users')
+const authRoute = require('./routes/auth')
+const postRoute = require('./routes/posts')
+
+// Connect DB
+mongoose.connect(process.env.DB_URL)
 
 // middleware
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(morgan('common'))
+
+// routes
+app.use('/api/users', userRoute)
+app.use('/api/auth', authRoute)
+app.use('/api/posts', postRoute)
 
 app.get('/', (req, res) => {
   res.send('Home page')
